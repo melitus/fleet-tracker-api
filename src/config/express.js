334e6b7eba69f218b17ba83b611697d9e624daa2
melitus <asmelitus@gmail.com>
2019-02-25 22:02:13 +0100

    created express config file

---
+++ src/config/express.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const passport = require('passport');
+
+const routes = require('../routes/index');
+const middlewaresConfig = require('../middlewares/middleConfig');
+const strategies = require('./passport');
+const error = require('../middlewares/error');
+
+// Express instance/
+const app = express();
+
+// body-parser defaults to a body size limit of 100kb
+app.use(express.json({ limit: '300kb' })); 
+
+// Trust the now proxy
+app.set('trust proxy', true);
+
+// Middlewares
+middlewaresConfig(app);
+
+// enable authentication
+app.use(passport.initialize());
+passport.use('jwt', strategies.jwt);
+
+// mount api routes
+app.use('/', routes);
+
+// if error is not an instanceOf APIError, convert it.
+app.use(error.converter);
+
+// catch 404 and forward to error handler
+app.use(error.notFound);
+
+// error handler, send stacktrace only during development
+app.use(error.handler);
+
+module.exports = app;
+
+
+
