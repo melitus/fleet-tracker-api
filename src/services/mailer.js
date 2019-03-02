46d464e42908283e0b8546ba7344a11fb9ca6253
melitus <asmelitus@gmail.com>
2019-03-02 21:22:45 +0100

    feat: created mail wrapper function

---
+++ src/services/mailer.js
@@ -0,0 +1,42 @@
+const winston = require('winston');
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+const  credential = require("../config/credentials")
+
+const smtpFromEmailConfig = credential.email;
+
+const getSmtp = () => {
+	const smtp = smtpFromEmailConfig;
+
+	return smtp;
+};
+
+const sendMail = (smtp, message) => {    
+	return new Promise((resolve, reject) => {
+		if (!message.to.includes('@')) {
+			reject('Invalid email address');
+			return;
+		}
+		const transporter = nodemailer.createTransport(smtpTransport(smtp));
+		transporter.sendMail(message, (err, info) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(info);
+			}
+		});
+	});
+};
+const send = async message => {
+	const smtp = getSmtp();
+	try {
+		const result = await sendMail(smtp, message);
+		winston.info('Email sent', result);
+		return true;
+	} catch (e) {
+		winston.error('Email send failed', e);
+		return false;
+	}
+};
+
+module.exports = send;
