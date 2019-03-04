77a4632f680ac44d190e489ec2d8c42fd181ea3e
melitus <asmelitus@gmail.com>
2019-03-04 23:09:13 +0100

    fixed:corrected otp verification

--- src/controllers/otp.controller.js
+++ src/controllers/otp.controller.js
@@ -5,7 +5,7 @@ exports.generateOtp = async (req, res, next) => {
   try {
     const otp = otpGenerator.generate(6, { digits: true, specialChars: false, alphabets: false, upperCase: false });
     const { email } = req.body;
-    const message = await User.FindOneAndUpdate({"email":email}, {"otp":otp} );
+    const message = await User.FindOneAndUpdate(email, {"otp":otp} );
     return res.send({ otp, message });
   } catch (err) {
     return next(err);
