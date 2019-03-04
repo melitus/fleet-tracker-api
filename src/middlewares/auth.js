01512390a90204fbb86299eea19c5fd7511b3a43
melitus <asmelitus@gmail.com>
2019-03-04 14:36:32 +0100

    created auth provider middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -50,4 +50,6 @@ exports.authorize = (roles = User.roles) => (req, res, next) =>
     handleJWT(req, res, next, roles),
   )(req, res, next);
 
+  exports.oAuth = service =>
+  passport.authenticate(service, { session: false });
 
