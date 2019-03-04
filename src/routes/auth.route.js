d535d3ca3d2100f9a16926e1578eac229d765b72
melitus <asmelitus@gmail.com>
2019-03-04 14:40:16 +0100

    added auth provider route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -16,5 +16,39 @@ router.route('/register')
 // auth/login Login
 router.route('/login')
   .post(validate(login), controller.login);
+  
+  //auth/facebook Facebook Login
+  router.route('/facebook')
+  .post(validate(oAuth), oAuthLogin('facebook'), controller.oAuth);
+
+  // auth/google Google Login
+  router.route('/google')
+  .post(validate(oAuth), oAuthLogin('google'), controller.oAuth);
+
 
 module.exports = router;
+
+
+router.route('/facebook')
+  .post(validate(oAuth), oAuthLogin('facebook'), controller.oAuth);
+
+/**
+ * @api {post} v1/auth/google Google Login
+ * @apiDescription Login with google. Creates a new user if it does not exist
+ * @apiVersion 1.0.0
+ * @apiName GoogleLogin
+ * @apiGroup Auth
+ * @apiPermission public
+ *
+ * @apiParam  {String}  access_token  Google's access_token
+ *
+ * @apiSuccess {String}  tokenType     Access Token's type
+ * @apiSuccess {String}  accessToken   Authorization Token
+ * @apiSuccess {String}  refreshToken  Token to get a new accpessToken after expiration time
+ * @apiSuccess {Number}  expiresIn     Access Token's expiration time in miliseconds
+ *
+ * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
+ * @apiError (Unauthorized 401)  Unauthorized    Incorrect access_token
+ */
+router.route('/google')
+  .post(validate(oAuth), oAuthLogin('google'), controller.oAuth);
