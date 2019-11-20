1d1d5e2f61a9c5c4560985428036cbe6144e5d69
melitus <asmelitus@gmail.com>
2019-11-20 18:18:18 +0100

    refine fleet services

---
+++ src/api/fleet/fleet.service.js
@@ -0,0 +1,59 @@
+/* eslint-disable prefer-promise-reject-errors */
+import * as mongoose from 'mongoose'
+
+import FleetModel from './fleet.model'
+
+export let ObjectId = mongoose.Schema.Types.ObjectId
+export let Mixed = mongoose.Schema.Types.Mixed
+
+// Load brand and append to req.
+const getSingleFleet = async fleetId => {
+  if (!mongoose.Types.ObjectId.isValid(fleetId)) {
+    return Promise.reject('Invalid identifier')
+  }
+  const fleet = await FleetModel.findById(fleetId).exec()
+  return fleet
+}
+
+const getFleets = async (params) => {
+  let pageNumber = parseInt(params.pagenumber)
+  const pageSize = parseInt(params.pagesize)
+  const searchQuery = {}
+  const fleets = await FleetModel.find(searchQuery)
+    .lean()
+    .sort({ createdAt: -1 })
+    .limit(pageSize)
+    .skip((pageNumber - 1) * pageSize)
+    .exec()
+  const fleetCount = await brandsCount()
+  const results = {
+    allFleets: fleets,
+    totalFleets: fleetCount
+  }
+  return results
+}
+const fleetsCount = async () => {
+  const allFleetsCount = await FleetModel.countDocuments({})
+    .lean()
+    .exec()
+  return allFleetsCount
+}
+export const addFleet = async (fleetData) => {
+  const newFleet = new FleetModel(fleetData)
+  const savedFleet = await newFleet.save()
+  return savedFleet
+}
+export const updateFleet = async (fleetId, fleetData) => {
+  if (!mongoose.Types.ObjectId.isValid(fleetId)) {
+    return Promise.reject('Invalid identifier')
+  }
+  const updatedFleet = await FleetModel.findByIdAndUpdate(fleetId, fleetData)
+  return updatedFleet
+}
+export const deleteBrand = async (fleetId) => {
+  if (!mongoose.Types.ObjectId.isValid(fleetId)) {
+    return Promise.reject('Invalid identifier')
+  }
+  const deletedFleet = await FleetModel.findByIdAndRemove(fleetId)
+  return deletedFleet
+}
