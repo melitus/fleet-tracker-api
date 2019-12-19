f4c07de2613a14b044298e0eeb6ba0bbd8cac9f1
melitus <asmelitus@gmail.com>
2019-12-19 18:01:20 +0100

    refine auth service

--- src/api/auth/auth.service.js
+++ src/api/auth/auth.service.js
@@ -25,12 +25,12 @@ const token = crypto.randomBytes(32).toString('hex')
 
 // Login Action
 
-export const checkUser = async email =>{
+exports.checkUser = async email =>{
     const user = await User.findOne({ email: email }, { password: 0, createdAt: 0, updatedAt: 0 }).exec()
     return user
   }
   
-  export const register = async userData => {
+  exports.register = async userData => {
     const user = new User(userData)
     const savedUser = await user.save()
     const userTransformed = savedUser.transform()
@@ -44,7 +44,7 @@ export const checkUser = async email =>{
     return response
   }
   
-  export const login = async userData => {
+  exports.login = async userData => {
     const { user, accessToken } = await User.findAndGenerateToken(userData)
     const token = generateTokenResponse(user, accessToken)
     const userTransformed = user.transform()
@@ -55,26 +55,11 @@ export const checkUser = async email =>{
     return response
   }
   
-  const logout = async (req) => {
+  exports.logout = async (req) => {
     req.logout();
   }
   
-  export const setLastPresentLogged = async userId=> {
-    const getTodayDate = new Date()
-    const user = await User.updateOne( {userId},
-      { $set:{ last_present_logged: getTodayDate}}
-    )
-    return user
-  }
-  export const setLastLogin = async userId => {
-    const getTodayDate = new Date()
-    const user = await User.updateOne( {userId},
-      { $set:{ lastLogin: getTodayDate}}
-    )
-    return user
-  }
-  
-  export const resendEmail = async email => {
+  exports.resendEmail = async email => {
     const savedUser = await User.findOne({ email: email })
     if (resendEmailVerification) {
       dispatch('resend-email', savedUser.uuid, savedUser, {
@@ -84,7 +69,7 @@ export const checkUser = async email =>{
     return savedUser
   }
   
-  export const getOTPCode = async data => {
+  exports.getOTPCode = async data => {
     const { phoneNo, email } = data
     const otpCode = await OTPWrapper.generateOTPCode()
     const isoPhoneNo = await OTPWrapper.getISOPhoneNo(phoneNo)
@@ -97,21 +82,21 @@ export const checkUser = async email =>{
     return message
   }
   
-  export const verifyOTPCode = async otpCode => {
+  exports.verifyOTPCode = async otpCode => {
     const otpVerified = await OTPWrapper.verifyOTPCode(otpCode)
     await User.verifyMobileOtp(otpCode)
     return otpVerified
   }
   
-  export const getOTPTimeUsed = () => {
+  exports.getOTPTimeUsed = () => {
     return OTPWrapper.getOTPtimeUsed()
   }
   
-  export const getOTPTimeRemaining = () => {
+  exports.getOTPTimeRemaining = () => {
     return OTPWrapper.getOTPtimeRemaining()
   }
   
-  export const oAuth = async req => {
+  exports.oAuth = async req => {
     const { user } = req
     const accessToken = user.token()
     const token = generateTokenResponse(user, accessToken)
@@ -120,7 +105,7 @@ export const checkUser = async email =>{
     return response
   }
   
-  export const refreshToken = async data => {
+  exports.refreshToken = async data => {
     const { email, refreshToken } = data
     const refreshObject = await RefreshToken.findOneAndRemove({
       userEmail: email,
@@ -134,74 +119,11 @@ export const checkUser = async email =>{
     return response
   }
   
-  export const forgotPassword = async data => {
-    const { email } = data
-    const user = await User.findOne({ email })
-    user.passwordResetToken = token
-    user.passwordResetExpires = issueExpiresDate // 15 minutes
-    const savedUser = await user.save()
-    const response = { token: token, user: savedUser }
-    if (sendResetPasswordMail) {
-      dispatch('reset-password', savedUser.uuid, savedUser, {
-        to: savedUser.email
-      })
-    }
-    return response
-  }
-  
-  export const verifyPasswordResetToken = async params => {
-    const { token } = params
-    const userToken = await User.findOne({
-      passwordResetToken: token,
-      passwordResetExpires: { $gt: Date.now() }
-    })
-    return userToken
-  }
-  
-  export const resetPassword = async data => {
-    const { password, userId } = data
-    const user = await User.findById(userId).exec()
-    const hashedPassword = await generatePasswordHash(password)
-    const fieldToUpdate = { password: hashedPassword }
-    await User.updateOne({ userId }, { $set: fieldToUpdate }, { new: true }).exec()
-    return user
-  }
-  
-  export const changePassword = async data => {
-    const { userId, oldpassword, newpassword } = data
-    const isUserExist = await User.findById(userId, { password: 1 }).exec()
-    const userPassword = isUserExist.password
-    const isPasswordCorrect = await passwordMatches(oldpassword, userPassword)
-    const hashedPassword = await generatePasswordHash(newpassword)
-    const fieldToUpdate = { password: hashedPassword }
-    await User.updateOne({ userId }, { $set: fieldToUpdate }, { new: true }).exec()
-    let response = {
-      isUserExist,
-      isPasswordCorrect
-    }
-    return response
-  }
-  export const isUserEmailExist = async email => {
+  exports.isUserEmailExist = async email => {
     const userEmailExist = await User.findOne({ email })
       .select('-password')
       .exec()
     return userEmailExist
   }
-  export const deactivateUserAccount = async userId => {
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { $set: { isActive: false } },
-      { fields: { _id: 0, isActive: 1 }, new: true }
-    )
-    return user
-  }
-  
-  export const activateUserAccount = async userId => {
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { $set: { isActive: true } },
-      { fields: { _id: 0, isActive: 1 }, new: true }
-    )
-    return user
-  }
+
  
